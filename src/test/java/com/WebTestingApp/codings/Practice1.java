1ea0713519e131331bc9cf3d9f1dc955c29e2ed3
anora1994 <82920621+anora1994@users.noreply.github.com>
2021-10-01 17:06:22 -0400

    AnorasBranch (#2)

--- src/test/java/com/WebTestingApp/codings/Practice1.java
+++ src/test/java/com/WebTestingApp/codings/Practice1.java
@@ -3,5 +3,6 @@ package com.WebTestingApp.codings;
 public class Practice1 {
     public static void main(String[] args) {
         System.out.println("word1");
+        System.out.println("word2");
     }
 }
